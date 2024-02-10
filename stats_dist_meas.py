7e9ee255d19976a3b27845f7f78a5e82cddf8bd9
Kahsolt <kahsolt@qq.com>
2024-02-10 16:52:17 +0800

    add stats_dist_meas

---
+++ stats_dist_meas.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+# Author: Armit
+# Create Time: 2024/02/10
+
+import json
+from argparse import ArgumentParser
+
+import matplotlib.pyplot as plt
+from skimage.metrics import mean_squared_error, normalized_root_mse, peak_signal_noise_ratio, structural_similarity
+
+from utils.img_proc import *
+from utils.niqe.niqe import get_niqe
+
+
+def plot_cmp(img_raw:PILImage, img_adv:PILImage):
+  img_raw_lo = img_raw.filter(ImageFilter.GaussianBlur(3))
+  img_adv_lo = img_adv.filter(ImageFilter.GaussianBlur(3))
+  im_raw_lo = pil_to_npimg(img_raw_lo)
+  im_adv_lo = pil_to_npimg(img_adv_lo)
+  im_raw = pil_to_npimg(img_raw)
+  im_adv = pil_to_npimg(img_adv)
+  im_raw_hi = minmax_norm(npimg_diff(im_raw, im_raw_lo))
+  im_adv_hi = minmax_norm(npimg_diff(im_adv, im_adv_lo))
+
+  print('[metrics]')
+  niqe_raw = get_niqe(im_raw) ; print('  niqe_raw:', niqe_raw)
+  niqe_adv = get_niqe(im_adv) ; print('  niqe_adv:', niqe_adv)
+  mse  = mean_squared_error     (im_raw, im_adv) ; print('  mse:', mse)
+  rmse = normalized_root_mse    (im_raw, im_adv) ; print('  rmse:', rmse)
+  psnr = peak_signal_noise_ratio(im_raw, im_adv) ; print('  psnr:', psnr)
+  ssim = structural_similarity  (to_gray(im_raw), to_gray(im_adv)) ; print('  ssim:', ssim)
+
+  dx    = npimg_abs_diff(im_adv,    im_raw,    name='dx')
+  dx_lo = npimg_abs_diff(im_adv_lo, im_raw_lo, name='dx_lo')
+  dx_hi = npimg_abs_diff(im_adv_hi, im_raw_hi, name='dx_hi')
+  dx0    = minmax_norm(dx, vmin=0, vmax=16)   # eps=16/255
+  dx0_lo = minmax_norm(dx_lo)
+  dx0_hi = minmax_norm(dx_hi)
+  dx1    = minmax_norm(to_ch_avg(dx), vmin=0, vmax=16)
+  dx1_lo = minmax_norm(to_ch_avg(dx_lo))
+  dx1_hi = minmax_norm(to_ch_avg(dx_hi))
+
+  plt.clf()
+  plt.subplot(3, 4,  1) ; plt.axis('off') ; plt.title('X')      ; plt.imshow(im_raw)
+  plt.subplot(3, 4,  2) ; plt.axis('off') ; plt.title('AX')     ; plt.imshow(im_adv)
+  plt.subplot(3, 4,  3) ; plt.axis('off') ; plt.title('DX3')    ; plt.imshow(dx0)
+  plt.subplot(3, 4,  4) ; plt.axis('off') ; plt.title('DX1')    ; plt.imshow(dx1)
+  plt.subplot(3, 4,  5) ; plt.axis('off') ; plt.title('X_lo')   ; plt.imshow(im_raw_lo)
+  plt.subplot(3, 4,  6) ; plt.axis('off') ; plt.title('AX_lo')  ; plt.imshow(im_adv_lo)
+  plt.subplot(3, 4,  7) ; plt.axis('off') ; plt.title('DX3_lo') ; plt.imshow(dx0_lo)
+  plt.subplot(3, 4,  8) ; plt.axis('off') ; plt.title('DX1_lo') ; plt.imshow(dx1_lo)
+  plt.subplot(3, 4,  9) ; plt.axis('off') ; plt.title('X_hi')   ; plt.imshow(im_raw_hi)
+  plt.subplot(3, 4, 10) ; plt.axis('off') ; plt.title('AX_hi')  ; plt.imshow(im_adv_hi)
+  plt.subplot(3, 4, 11) ; plt.axis('off') ; plt.title('DX3_hi') ; plt.imshow(dx0_hi)
+  plt.subplot(3, 4, 12) ; plt.axis('off') ; plt.title('DX1_hi') ; plt.imshow(dx1_hi)
+  plt.show()
+
+
+def stats_cmp(img_raw:PILImage, img_adv:PILImage) -> Any:
+  im_raw = pil_to_npimg(img_raw)
+  im_adv = pil_to_npimg(img_adv)
+
+  niqe_raw = get_niqe(im_raw)
+  niqe_adv = get_niqe(im_adv)
+  mse  = mean_squared_error     (im_raw, im_adv)
+  rmse = normalized_root_mse    (im_raw, im_adv)
+  psnr = peak_signal_noise_ratio(im_raw, im_adv)
+  ssim = structural_similarity  (to_gray(im_raw), to_gray(im_adv))
+
+  im_raw_f32 = im_raw.astype(np.float32) / 255.0
+  im_adv_f32 = im_adv.astype(np.float32) / 255.0
+
+  dx = np.abs(im_adv_f32 - im_raw_f32)
+  Linf = dx.max()
+  L1 = dx.mean()
+  L2 = np.linalg.norm(dx.flatten(), ord=2)
+
+  return {
+    'Linf': Linf.item(),
+    'L1': L1.item(),
+    'L2': L2.item(),
+    'mse': mse,
+    'rmse': rmse,
+    'psnr': psnr,
+    'ssim': ssim,
+    'niqe+': niqe_adv - niqe_raw,   # diff value
+    'niqe_raw': niqe_raw,
+    'niqe_adv': niqe_adv,
+  }
+
+
+def run(args):
+  dp_raw: Path = args.input
+  dp_adv: Path = args.output
+
+  adv_fps = list(dp_adv.iterdir())    # keep numerical order
+  adv_fps.sort(key=(lambda x: int(x.stem)))
+
+  results = {}
+  for fp_adv in tqdm(adv_fps):
+    fp_raw = dp_raw / fp_adv.name
+
+    img_raw = load_img(fp_raw)
+    img_adv = load_img(fp_adv)
+    #plot_cmp(img_raw, img_adv)
+    stats = stats_cmp(img_raw, img_adv)
+    results[fp_adv.name] = stats
+
+  stats = {}
+  metric_names = list(list(results.values())[0].keys())
+  for metric in metric_names:
+    data = np.asarray([v[metric] for v in results.values()])
+    stats[metric] = {
+      'avg': data.mean(),
+      'std': data.std(),
+      'max': data.max(),
+      'min': data.min(),
+    }
+
+  print(f'>> save to {args.save_fp}')
+  with open(args.save_fp, 'w', encoding='utf-8') as fh:
+    data = {
+      'stats': stats,
+      'results': results,
+    }
+    json.dump(data, fh, ensure_ascii=False, indent=2)
+
+
+if __name__ == '__main__':
+  parser = ArgumentParser()
+  parser.add_argument('-I', '--input',   type=Path, default=DATA_RAW_PATH, help='clean image folder')
+  parser.add_argument('-O', '--output',  type=Path, default=DATA_ADV_PATH, help='adv image folder')
+  parser.add_argument('-S', '--save_fp', type=Path, default=(LOG_PATH / 'stats_dist_meas.json'), help='result output file')
+  args = parser.parse_args()
+
+  run(args)
---
+++ utils/img_proc.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+# Author: Armit
+# Create Time: 2022/09/27 
+
+import warnings ; warnings.filterwarnings('ignore', category=UserWarning)
+
+from pathlib import Path
+from PIL import Image, ImageFilter
+from PIL.Image import Image as PILImage
+from typing import *
+
+import torch
+from torch import Tensor
+import numpy as np
+from numpy import ndarray
+from numpy.typing import NDArray
+from tqdm import tqdm
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+BASE_PATH = Path(__file__).parent.parent
+LOG_PATH = BASE_PATH / 'log' ; LOG_PATH.mkdir(exist_ok=True)
+DATA_PATH = BASE_PATH / 'dataset'
+DATA_RAW_PATH = DATA_PATH / 'NIPS17'
+DATA_ADV_PATH = DATA_PATH / 'ssa-cwa-200' # pregen
+
+IM_U8_TYPES = ['u', 'u8', 'uint8', np.uint8]
+IM_F32_TYPES = ['f', 'f32', 'float32', np.float32]
+IM_TYPES = IM_U8_TYPES + IM_F32_TYPES
+
+npimg_u8 = NDArray[np.uint8]        # vrng [0, 255]
+npimg_f32 = NDArray[np.float32]     # vrng [0, 1]
+npimg = Union[npimg_u8, npimg_f32]
+npimg_dx = NDArray[np.int16]        # vrng [-255, 255]
+npimg_hi = NDArray[np.float32]      # vrng [-1, 1]
+
+
+def load_img(fp:Path) -> PILImage:
+  return Image.open(fp).convert('RGB')
+
+def load_im(fp:Path, dtype:str='u8') -> npimg:
+  return pil_to_npimg(load_img(fp), dtype)
+
+def pil_to_npimg(img:PILImage, dtype:str='u8') -> npimg:
+  assert dtype in IM_TYPES
+  im = np.asarray(img, dtype=np.uint8)
+  if dtype in IM_U8_TYPES: return im
+  return im.astype(np.float32) / 255.0
+
+def npimg_to_pil(im:npimg) -> PILImage:
+  assert im.dtype in IM_TYPES
+  if im.dtype in IM_F32_TYPES:
+    assert 0.0 <= im.min() and im.max() <= 1.0
+  return Image.fromarray(im)
+
+def hwc2chw(im:npimg) -> npimg:
+  return im.transpose(2, 0, 1)
+
+def chw2hwc(im:npimg) -> npimg:
+  return im.transpose(1, 2, 0)
+
+def npimg_to_tensor(im:npimg_f32) -> Tensor:
+  return torch.from_numpy(hwc2chw(im))
+
+def to_gray(im:npimg) -> npimg:
+  return pil_to_npimg(npimg_to_pil(im).convert('L'))
+
+def to_ch_avg(x:ndarray) -> ndarray:
+  return np.tile(x.mean(axis=-1, keepdims=True), (1, 1, 3))
+
+def npimg_abs_diff(x:npimg, y:npimg, name:str=None) -> npimg:
+  d: ndarray = np.abs(npimg_diff(x, y))
+  if name:
+    print(f'[{name}]')
+    print('  Linf:', d.max() / 255)
+    print('  L1:',  d.mean() / 255)
+  return d
+
+def npimg_diff(x:npimg_u8, y:npimg_u8) -> npimg_dx:
+  return x.astype(np.int16) - y.astype(np.int16)
+
+def minmax_norm(dx:npimg_dx, vmin:int=None, vmax:int=None) -> npimg_u8:
+  vmin = vmin or dx.min()
+  vmax = vmax or dx.max()
+  out = (dx - vmin) / (vmax - vmin)
+  return (out * 255).astype(np.uint8)
+
+def Linf_L1_L2(X:Tensor, AX:Tensor=None) -> Tuple[float, float, float]:
+  if AX is None:
+    DX = X
+  else:
+    DX = (AX - X).abs()
+  Linf = DX.max()
+  L1 = DX.mean()
+  L2 = (DX**2).sum().sqrt()
+  return [x.item() for x in [Linf, L1, L2]]
