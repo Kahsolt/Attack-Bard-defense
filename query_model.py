a9f70e71ca8f5a4cab73e4da6717aea4ead6c8b1
Kahsolt <kahsolt@qq.com>
2024-08-03 10:12:20 +0800

    amend query_model

--- query_model.py
+++ query_model.py
@@ -1,37 +1,51 @@
-import requests
 from PIL import Image
 
 import torch
-from transformers import AutoProcessor, LlavaForConditionalGeneration
+from transformers import LlavaForConditionalGeneration, LlavaProcessor
 
-model_id = "llava-hf/llava-1.5-7b-hf"
+device = 'cuda'
+
+#pretrained_model_name_or_path = "llava-hf/llava-1.5-7b-hf"
+pretrained_model_name_or_path = r'C:\Users\jiangsongru\.cache\huggingface\hub\models--llava-hf--llava-1.5-7b-hf\snapshots\9e7ca49a0174c7cc68c2605d221b4cac0aea799d'
 model = LlavaForConditionalGeneration.from_pretrained(
-    model_id, 
-    pretrained_model_name_or_path='./weights-7b/',
-    torch_dtype=torch.float16, 
-    low_cpu_mem_usage=True, 
-).to(0)
-
-processor = AutoProcessor.from_pretrained(model_id, 
-                                          pretrained_model_name_or_path='./weights-7b/')
+  pretrained_model_name_or_path, 
+  torch_dtype=torch.float16, 
+  low_cpu_mem_usage=True, 
+  #load_in_8bit=True,
+  load_in_4bit=True,
+  #use_flash_attention_2=True,
+)#.to('cuda')
+processor = LlavaProcessor.from_pretrained(
+  pretrained_model_name_or_path, 
+)
 
 # Define a chat histiry and use `apply_chat_template` to get correctly formatted prompt
 # Each value in "content" has to be a list of dicts with types ("text", "image") 
 conversation = [
-    {
-
-      "role": "user",
-      "content": [
-          {"type": "text", "text": "What are these?"},
-          {"type": "image"},
-        ],
-    },
+  {
+    "role": "user",
+    "content": [
+      {"type": "text", "text": "What are these?"},
+      {"type": "image"},
+    ],
+  },
 ]
 prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
 
-image_file = "http://images.cocodataset.org/val2017/000000039769.jpg"
-raw_image = Image.open(requests.get(image_file, stream=True).raw)
-inputs = processor(prompt, raw_image, return_tensors='pt').to(0, torch.float16)
 
-output = model.generate(**inputs, max_new_tokens=200, do_sample=False)
-print(processor.decode(output[0][2:], skip_special_tokens=True))
+try:
+  while True:
+    fp = input('>> input file path: ').strip()
+
+    try:
+      raw_image = Image.open(fp).convert('RGB')
+    except Exception as e:
+      print(e)
+      continue
+
+    inputs = processor(prompt, raw_image, return_tensors='pt').to(device, torch.float16)
+    output = model.generate(**inputs, max_new_tokens=200, do_sample=False)
+    result = processor.decode(output[0][2:], skip_special_tokens=True)
+    print(result)
+except KeyboardInterrupt:
+  pass
